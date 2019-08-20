d4c8a14affbf89edfd6718d13903538f712dbac8
sunhaixin <1113285307@qq.com>
2019-08-20 11:05:27 +0800

    处理请求 header

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 const axios = (config: AxiosRequestConfig): void => {
   processConfig(config)
@@ -10,12 +11,18 @@ const axios = (config: AxiosRequestConfig): void => {
 
 const processConfig = (config: AxiosRequestConfig): void => {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
 const transformUrl = (config: AxiosRequestConfig): string => {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 const transformRequestData = (config: AxiosRequestConfig): any => {
